de8f8de26374c344ad0966cf4e5e2866dc4724f4
Gemma <gemma.castaneda9048@alumnos.udg.mx>
2023-09-24 20:41:53 -0600

    Inicio del proyecto

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <SimpleCounter />
+  </div>
+</template>
+
+<script>
+import SimpleCounter from './components/SimpleCounter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SimpleCounter
+  }
+}
+</script>
+
+<style>
+/* Estilos generales de la aplicación */
+</style>
---
+++ src/components/SimpleCounter.vue
@@ -0,0 +1,64 @@
+<!-- src/components/SimpleCounter.vue -->
+
+/* eslint-disable */
+<template>
+    <div class="container">
+      <h1>Contador: {{ count }}</h1>
+      <button @click="increaseCount">Aumentar</button>
+      <button @click="decreaseCount">Disminuir</button>
+      <button @click="resetCount">Restablecer</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SimpleCounter',
+    data() {
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      increaseCount() {
+        this.count++;
+      },
+      decreaseCount() {
+        if (this.count > 0) {
+          this.count--;
+        }
+      },
+      resetCount() {
+        this.count = 0;
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .container {
+    background-image: linear-gradient(to right, pink, purple);
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  button {
+    background-color: #4CAF50; /* Verde */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 8px;
+  }
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+  * {
+  font-family: 'Roboto', sans-serif;
+  }
+  </style>
+  
